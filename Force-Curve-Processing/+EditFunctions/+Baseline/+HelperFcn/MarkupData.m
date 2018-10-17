b336c4bb2f841ad5f835af79dc9f50acf429ef55
julianstar88 <jblaser@hm.edu>
2018-10-17 23:10:31 +0200

    changed ConvertToVector from nested function to HelperFcn
    # Main Update:
    
    changed the nested ConvertToVector-function to an own HelperFcn
    
    # Minor Updates
    
    **MarkupData:**
    - begin adding an TransfomToAbsolute function for transformation of
      relative values
    
    **UpdateElemtentsAccordingToUnitsCallback:**
    - updated path to ConvertToVector function

--- Force-Curve-Processing/+EditFunctions/+Baseline/+HelperFcn/MarkupData.m
+++ Force-Curve-Processing/+EditFunctions/+Baseline/+HelperFcn/MarkupData.m
@@ -29,7 +29,29 @@ function MarkupData()
     
     function absolute = TransformToAbsolute(results, handles)
         
-        % do calculations
+        % preparation of needed variables
+        table = handles.guiprops.Features.edit_curve_table;
+        xchannel = handles.guiprops.Features.curve_xchannel_popup.Value;
+        ychannel = handles.guiprops.Features.curve_ychannel_popup.Value;
+        curvename = table.UserData.CurrentCurveName;
+        RawData = handles.curveprops.(curvename).RawData;
+        editfunctions = allchild(handles.guiprops.Panels.processing_panel);
+        baseline = findobj(editfunctions, 'Tag', 'Baseline');
+        last_editfunction_index = find(editfunctions == baseline) + 1;
+        last_editfunction = editfunctions(last_editfunction_index).Tag;
+        
+        % transformation of borders
+            x = linedata(:,1);
+
+            % left border
+            a_left_index = round(length(x)*old_borders(1));
+            a_left = x(a_left_index);
+
+            % right border
+            a_right_index = round(length(x)*old_borders(2));
+            a_right = x(a_right_index);
+
+            new_borders = [a_left a_right];
         
     end % TransformToAbsolute
 end % MarkupData
