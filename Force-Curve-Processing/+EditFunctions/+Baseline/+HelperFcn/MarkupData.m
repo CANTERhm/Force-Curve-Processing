e36814c4305e68f9d5b7b4912ab85057c279b3e4
julianstar88 <jblaser@hm.edu>
2018-10-17 15:54:57 +0200

    proceed on MarkupData
    plus: test-callback updated

--- Force-Curve-Processing/+EditFunctions/+Baseline/+HelperFcn/MarkupData.m
+++ Force-Curve-Processing/+EditFunctions/+Baseline/+HelperFcn/MarkupData.m
@@ -4,7 +4,33 @@ function MarkupData()
 %   propertylistener and marks up the range according to this property
 %   on FCP GraphWindow.
 
+    %% refresh handles and results
+    main = findobj(allchild(groot), 'Type', 'Figure', 'Tag', 'figure1');
+    handles = guidata(main);
+    results = getappdata(main, 'Baseline');
+    
+    %% markup datarange
+    
+    % transform relative units to absolute
+    if strcmp(results.units, 'relative')
+        
+    end
+    
+    
+    %% update handles, results and fire event UpdateObject
+    % update results object
+    setappdata(handles.figure1, 'Baseline', results);
+    guidata(handles.figure1, handles);
 
-
+    % trigger update to handles.curveprops.curvename.Results.Baseline
+    results.FireEvent('UpdateObject');
+    
+    %% nested functions
+    
+    function absolute = TransformToAbsolute(results, handles)
+        
+        % do calculations
+        
+    end % TransformToAbsolute
 end % MarkupData
 
