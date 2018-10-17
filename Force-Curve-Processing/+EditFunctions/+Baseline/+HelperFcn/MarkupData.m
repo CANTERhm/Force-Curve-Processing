3a2630e64633b6aa100dbafb6911696016c3105b
julianstar88 <jblaser@hm.edu>
2018-10-17 15:39:22 +0200

    Added MarupData Function

---
+++ Force-Curve-Processing/+EditFunctions/+Baseline/+HelperFcn/MarkupData.m
@@ -0,0 +1,10 @@
+function MarkupData()
+%MARKUPDATA Marksup data between selection_borders property
+%  	Tracks the selection_borders property via
+%   propertylistener and marks up the range according to this property
+%   on FCP GraphWindow.
+
+
+
+end % MarkupData
+
