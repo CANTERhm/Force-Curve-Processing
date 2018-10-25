1dfcbe40cc11d8bf05ff64cdc13d6e0f7aa60b4a
julianstar88 <jblaser@hm.edu>
2018-10-25 12:58:20 +0200

    update UpdateOffsetCallback

--- Force-Curve-Processing/+EditFunctions/+Baseline/+Callbacks/UpdateOffsetCallback.m
+++ Force-Curve-Processing/+EditFunctions/+Baseline/+Callbacks/UpdateOffsetCallback.m
@@ -1,7 +1,34 @@
-function [outputArg1,outputArg2] = UpdateOffsetCallback(inputArg1,inputArg2)
-%UPDATEOFFSETCALLBACK Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function UpdateOffsetCallback(src, evt)
+%UPDATEOFFSETCALLBACK Update offset_value_label and offset_unit_label
+%according to results-object
+
+    % get results-object
+    main = findobj(allchild(groot), 'Type', 'Figure', 'Tag', 'figure1');
+    handles = guidata(main);
+    results = getappdata(handles.figure1, 'Baseline');
+    
+    % get label references
+    off_value = results.results_features.offset_value_label;
+    off_unit = results.results_features.offset_unit_label;
+    
+    % update value labels
+    off_value.String = num2str(results.offset);
+    
+    % update unit labels
+    table = handles.guiprops.Features.edit_curve_table;
+    curvename = table.UserData.CurrentCurveName;
+    units = handles.curveprops.(curvename).RawData.SpecialInformation.Segment1.units;
+    if ~isempty(units)
+        xch = handles.guiprops.Features.curve_xchannel_popup.Value;
+        off_unit.String = units{xch};
+    end
+
+    % refresh results object and handles
+    setappdata(handles.figure1, 'Baseline', results);
+    guidata(handles.figure1, handles);
+
+    % trigger update to handles.curveprops.curvename.Results.Baseline
+    results.FireEvent('UpdateObject');
+
+end % UpdateOffsetCallback
 
