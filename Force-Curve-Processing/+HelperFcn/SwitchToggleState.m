841abc150f91498db2433d19ac2a14323efeefc1
julianstar88 <jblaser@hm.edu>
2018-11-14 23:11:36 +0100

    fixed: SwitchToggleState
    now Results-object for determining if Editfunctionelements are on gui

--- Force-Curve-Processing/+HelperFcn/SwitchToggleState.m
+++ Force-Curve-Processing/+HelperFcn/SwitchToggleState.m
@@ -7,52 +7,108 @@ function SwitchToggleState(src, varargin)
 %   - obj_list: optional property, a cell-array, containing a list of
 %   switchable gui elements, which shall be switched off if src is set true
 
-%% input parsing
-p = inputParser;
+    %% input parsing
+    p = inputParser;
 
-ExpectedStyles = {'togglebutton', 'checkbox'};
-ValidStyles = @(x)assert(any(validatestring(x, ExpectedStyles)), 'SwitchToggleState:wrongStyle',...
-    'input was not an expected style for toggle-element.');
+    ExpectedStyles = {'togglebutton', 'checkbox'};
+    ValidStyles = @(x)assert(any(validatestring(x, ExpectedStyles)), 'SwitchToggleState:wrongStyle',...
+        'input was not an expected style for toggle-element.');
 
-addRequired(p, 'src');
-addOptional(p, 'obj_list', []);
-addParameter(p, 'style', 'togglebutton', ValidStyles)
+    addRequired(p, 'src');
+    addOptional(p, 'obj_list', []);
+    addParameter(p, 'style', 'togglebutton', ValidStyles)
 
-parse(p, src, varargin{:});
+    parse(p, src, varargin{:});
 
-src = p.Results.src;
-obj_list = p.Results.obj_list;
-style = p.Results.style;
+    src = p.Results.src;
+    obj_list = p.Results.obj_list;
+    style = p.Results.style;
 
-%% function procedure
-
-switch style
-    case 'togglebutton' % make sure, only switching another togglebutton can deactivate src
-        if src.Value == 0
-            src.Value = 1;
-        end
-    case 'checkbox' % every other checkbox gets unchecked
-        if src.Value == 0
-            src.Value = 1;
-        else
-            src.Value = 0;
-        end
-end
-
-% if src.Value == 1, set every other toggleelemt.Value == 0
-if isempty(obj_list)
-    list = allchild(src.Parent);
-else
-    list = obj_list;
-end
-mask = list ~= src;
-list = list(mask);
-for i = 1:length(list)
-    if ~isa(list, 'cell')
-        list(i).Value = 0;
+    %% function procedure
+    
+    % work off obj_list input parameter
+    if isempty(obj_list)
+        list = allchild(src.Parent);
     else
-        list{i}.Value = 0;
+        list = obj_list;
     end
-end 
+    
+    % catch src.Value before it gets changed
+    if isa(src.UserData, 'struct')
+        names = fieldnames(src.UserData);
+        if ~any(ismember(names, 'lh'))
+            src.UserData.lh = [];
+        end
+        if ~any(ismember(names, 'last_value'))
+            src.UserData.last_value = [];
+        end
+        if ~any(ismember(names, 'on_gui'))
+            src.UserData.on_gui = Results();
+            src.UserData.on_gui.Status = false;
+        end
+    else
+        src.UserData.lh = [];
+        src.UserData.last_value = [];
+        src.UserData.on_gui = Results();
+        src.UserData.on_gui.Status = false;
+    end
+    
+    if isempty(src.UserData.lh)
+        src.UserData.lh = PropListener();
+        src.UserData.lh.addListener(src, 'Value', 'PreSet', @SetLastValue);
+        src.UserData.lh.addListener(src, 'Value', 'PreSet', @SetOnGui);
+    end
+    
+    % work off style input parameter
+    switch style
+        case 'togglebutton' % make sure, only switching another togglebutton can deactivate src
+            if src.Value == 0 
+                % if src isn't pressed, press it
+                src.Value = 1;
+            end
+        case 'checkbox' % every other checkbox gets unchecked
+            if src.Value == 0
+                src.Value = 1;
+            else
+                src.Value = 0;
+            end
+    end
 
+    % if src.Value == 1, set every other toggleelemt.Value = 0
+    mask = list ~= src;
+    list = list(mask);
+    for i = 1:length(list)
+        if isa(list, 'cell')
+            list{i}.Value = 0;
+        else
+            list(i).Value = 0;
+        end
+    end 
+    
+    %% Callbacks
+    
+    function SetLastValue(src, evt)
+        obj = evt.AffectedObject;
+        obj.UserData.last_value = obj.Value;
+    end
+
+    function SetOnGui(src, evt)
+        obj = evt.AffectedObject;
+        try
+            last_value = obj.UserData.last_value;
+        catch
+            last_value = [];
+        end
+        if ~isempty(last_value)
+            if obj.Value == 0 && obj.UserData.last_value == 1
+                obj.UserData.on_gui.Status = true;
+            else
+                obj.UserData.on_gui.Status = false;
+            end
+        else
+            obj.UserData.on_gui.Status = false;
+        end
+    end
+
+end % SwitchToggleState
 
--- Force-Curve-Processing/test.m
+++ Force-Curve-Processing/test.m
@@ -1,18 +1,58 @@
+close all
 fig = figure();
 panel = uix.Panel('Parent', fig);
-box = uix.Grid('Parent', panel);
+box = uix.HButtonBox('Parent', panel);
 btn1 = uicontrol('Parent', box,...
-    'Style', 'pushbutton',...
-    'String', 'test1');
+    'Style', 'togglebutton',...
+    'String', 'test1',...
+    'Callback', @cb);
+
 btn2 = uicontrol('Parent', box,...
-    'Style', 'pushbutton',...
-    'String', 'test2');
+    'Style', 'togglebutton',...
+    'String', 'test2',...
+    'Callback', @cb);
 btn3 = uicontrol('Parent', box,...
-    'Style', 'pushbutton',...
-    'String', 'test3');
-label = TextLabel('Parent', box,...
-    'String', 'TestLabel',...
-    'HorizontalAlignment', 'center',...
-    'VerticalAlignment', 'center');
-box.Widths = [-1 -1];
-box.Heights = [-1 -1];
+    'Style', 'togglebutton',...
+    'String', 'test3',...
+    'Callback', @cb);
+
+
+function cb(src, evt)
+    HelperFcn.SwitchToggleState(src);
+    ExplicitlyNoCallback();
+    par = src.Parent;
+    list = allchild(par);
+    fmt = 'Object: %s;  Is on Gui: %d\n';
+    clc
+    for i = 1:length(list)
+        try
+            part1 = list(i).String;
+        catch
+            part1 = 999;
+        end
+        try
+            part2 = list(i).UserData.on_gui.Status;
+        catch
+            part2 = 999;
+        end
+        fprintf(fmt, part1, part2);
+    end
+end
+
+function ExplicitlyNoCallback()
+    % gets called inside an callback
+    button = findall(allchild(groot), 'Type', 'UIControl');
+    btn1 = button(1);
+    lh = PropListener();
+    try
+        lh.addListener(btn1.UserData.on_gui, 'Status', 'PostSet', @lcb);
+    catch
+        % move on
+    end
+    
+    function lcb(src, evt)
+        obj = evt.AffectedObject;
+    end
+end
+
+
