cba4b0c8f82f6c4c5cfa9b64d0b9815bdda33191
julianstar88 <jblaser@hm.edu>
2018-11-16 15:21:40 +0100

    Added Version-Number
    From now on the ForceCurveProcessing-App gets tracked via Version-Number.
    Start here is Version 0.0.0; the reason is that it is not completely working so far

--- Force-Curve-Processing/ForceCurveProcessing.m
+++ Force-Curve-Processing/ForceCurveProcessing.m
@@ -1,4 +1,7 @@
 function varargout = ForceCurveProcessing(varargin)
+%==========================================================================
+% This is Version: 0.0.0
+%==========================================================================
 % FORCECURVEPROCESSING MATLAB code for ForceCurveProcessing.fig
 %      FORCECURVEPROCESSING, by itself, creates a new FORCECURVEPROCESSING or raises the existing
 %      singleton*.
